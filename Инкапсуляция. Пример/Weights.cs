dc96c267cdfd783f13000d71d41f1ce37c6cdca0
tako24 <frenk6795@gmail.com>
2021-09-30 20:09:52 +0500

    Create Weights.cs

---
+++ Инкапсуляция. Пример/Weights.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incapsulation.Weights
+{
+    class Indexer
+    {
+        readonly double[] array;
+        public int Length { get; }
+        public int Start { get; }
+        public Indexer(double[] array, int start, int length)
+        {
+            if (start < 0 || length < 0 || length > array.Length || length > array.Length - start)
+                throw new ArgumentException();
+            Start = start;
+            Length = length;
+            this.array = array;
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > Length - 1)
+                    throw new IndexOutOfRangeException();
+                return array[Start + index];
+            }
+            set
+            {
+                if (index < 0 || index > Length - 1)
+                    throw new IndexOutOfRangeException();
+                array[Start + index] = value;
+            }
+        }
+    }
+}
